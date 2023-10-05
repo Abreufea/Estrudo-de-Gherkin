3bd0dd4adfb61648f2764510d987a4969c727f24
Fernando de Abreu <abreufea@gmail.com>
2023-10-05 15:20:57 -0300

    aprimoramento do teste com o wrap

--- cypress/support/step_definitions/cart-page-stpes.js
+++ cypress/support/step_definitions/cart-page-stpes.js
@@ -3,20 +3,19 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 When('I acess the cart page', () => {
     cy.get('.shopping_cart_link').click();
 })
-When('I remeve all itens from cart shopping', () => {
 
-    cy.get('[data-test="remove-sauce-labs-backpack"]').click();
-    cy.get('[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
-    cy.get('[data-test="remove-sauce-labs-fleece-jacket"]').click();
-    cy.get('[data-test="remove-sauce-labs-onesie"]').click();
-    cy.get('[data-test="remove-test.allthethings()-t-shirt-(red)"]').click();
-})
 Then('I have a empy Cart and comeback to shopping', () => {
     cy.get('#continue-shopping').click();
 })
 
 When('I add all store items to my shopping cart', () => {
     cy.get('.btn_primary.btn_inventory').each(($element) => {
+        cy.wrap($element).click();
+    });
+})
+
+When('I remeve all itens from cart shopping', () => {
+    cy.get('.btn_small.cart_button').each(($element) => {
         cy.wrap($element).click();
     });
 })
